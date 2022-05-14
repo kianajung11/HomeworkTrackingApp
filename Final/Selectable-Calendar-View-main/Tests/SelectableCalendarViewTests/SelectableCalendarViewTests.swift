70281d45713dbda33453473096d50a2ae4f50fb7
kianajung11 <78405079+kianajung11@users.noreply.github.com>
2022-05-13 19:34:35 -0700

    Delete Final directory

--- Final/Selectable-Calendar-View-main/Tests/SelectableCalendarViewTests/SelectableCalendarViewTests.swift
+++
@@ -1,8 +0,0 @@
-import XCTest
-@testable import SelectableCalendarView
-
-final class SelectableCalendarViewTests: XCTestCase {
-    func testExample() throws {
-        XCTAssert(true)
-    }
-}
--- Final/Shared/Views/Profile.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Profile.swift
-//  Final
-//
-//  Created by Kiana Jung on 5/12/22.
-//
-
-import Foundation
